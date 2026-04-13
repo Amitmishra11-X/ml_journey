3ce9772788ef1341deb42606b04ef465477b05f1
Amit Mishra <am0651465@gmail.com>
2026-04-13 13:11:26 +0530

    changes

--- panda.py
+++ panda.py
@@ -7,3 +7,6 @@ df = pd.DataFrame({'Rohan': ['Item is good', 'okk'],
                    'Ram': ['good', 'okk']},
                   index=['product A', 'product B'])
 print(df)
+import pandas as pd
+df= pd.Series([1, 2, 3, 4, 5], index = ['2025 sales', '2026 sales', '2024 sales', '2023 sales', '2022 sales'], name = 'product A')
+print(df)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df= pd.Series([1, 2, 3, 4, 5], index = ['2025 sales', '2026 sales', '2024 sales', '2023 sales', '2022 sales'], name = 'product A')
+print(df)
